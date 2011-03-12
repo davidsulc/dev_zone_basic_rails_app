adb726e1a5d908953511164343b8ed2b798c5be8
davidsulc <david@davidsulc.com>
2011-03-12 17:39:18 -0500

    generate new rails app
    command used: `rails new rails_demo --skip-active-record
    (we won't be using a database)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDemoModel::Application.config.secret_token = '44b237d2b093433c7bbd93722f4c79964c4725df3adafe7889124c4eb843e7fce9277acd278646ce775021cd7696595610847b8cea75ae964aa78b093f8e4cd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDemoModel::Application.config.session_store :cookie_store, :key => '_rails_demo_model_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# RailsDemoModel::Application.config.session_store :active_record_store
